cca69500424109a6e7a74ac88ac780d4fb1771e1
colin-woon <colinwyj@gmail.com>
2023-11-17 17:00:25 +0800

    Created and tested a minimal http server
    - Imported http module
    - Created PORT
    - Created server
    - Added server listener
    - Tested to check for GET request

--- nodejs-webserver/server.js
+++ nodejs-webserver/server.js
@@ -1,16 +1,26 @@
-const logEvents = require('./logEvents')
+const http = require('http');
+const path = require('path')
+const fs = require('fs')
+const fsPromises = require('fs').promises
 
+const logEvents = require('./logEvents')
 const EventEmitter = require('events')
 
 class Emitter extends EventEmitter { };
-
-//initialize object, seperated this way to promote code reusability and encourage the principle of seperation of concerns
+//--Initialize object, seperated this way to promote code reusability and encourage the principle of seperation of concerns
 const myEmitter = new Emitter();
 
-//add listener for the log event
-myEmitter.on('log', (msg) => logEvents(msg));
+//--If hosted on another server, will have own PORT, OR, just use 3500
+const PORT = process.env.PORT || 3500
+
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method);
+});
+
+//--MUST Add a http request listener at the end of .js file
+server.listen(PORT, () => console.log(`Server is listening, running on port ${PORT}`));
 
 //--For practicing emit message for listener to detect 
-// setTimeout(() => {
+// //--Add listener for the log event
+// myEmitter.on('log', (msg) => logEvents(msg));
 //     myEmitter.emit('log', 'Log event emitted');
-// }, 2000);
