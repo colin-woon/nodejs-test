622018854a5e02e65d26d22fffc2f353cad327c6
colin-woon <colinwyj@gmail.com>
2023-11-21 14:20:48 +0800

    Implemented express.js
    - practiced regex expressions
    - chaining route handlers
    - setting status code 404

--- f.express/server.js
+++ f.express/server.js
@@ -1,122 +1,52 @@
-const http = require('http');
-const path = require('path')
-const fs = require('fs')
-const fsPromises = require('fs').promises
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 3500;
 
-const logEvents = require('./logEvents')
-const EventEmitter = require('events')
+//--^ = Begin with
+//--$ = End with
+//--| = OR
+//--(<words>)? = Optional
+app.get('^/$|/index(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+})
 
-class Emitter extends EventEmitter { };
-//--Initialize object, seperated this way to promote code reusability and encourage the principle of seperation of concerns
-const myEmitter = new Emitter();
-myEmitter.on('log', (msg, fileName) => logEvents(msg, fileName));
-//--If hosted on another server, will have own PORT, OR, just use 3500
-const PORT = process.env.PORT || 3500
+app.get('/new-page.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
+})
 
-const serveFile = async (filePath, contentType, res) => {
-    try {
-        //--Check contentType for image while reading file, utf8 is not image
-        const rawData = await fsPromises.readFile(filePath, contentType.includes('image') ? '' : 'utf8');
-        //--Check data type for JSON
-        const data = contentType === 'application/json'
-            ? JSON.parse(rawData) : rawData;
-        res.writeHead(
-            filePath.includes('404.html') ? 404 : 200, //Setting 404 status code
-            { 'Content-Type': contentType });
-        //--Sends the data back
-        res.end(contentType === 'application/json' ? JSON.stringify(data) : data);
-    } catch (error) {
-        console.log(error);
-        myEmitter.emit('log', `${error.name}\t${error.message}`, 'errorLog.txt');
-        res.statusCode = 500;
-        res.end();
-    }
+app.get('/fake-page.html', (req, res) => {
+    res.redirect(301, '/new-page.html'); //302 by default (301 means permanently redirect)
+})
+
+//--Route Handlers
+app.get('/hello(.html)?', (req, res, next) => {
+    console.log("tried to load hello.html");
+    next();
+}, (req, res) => { //--Not a common way to chain to another handler
+    res.send('Hello World I was alive');
+})
+
+//--Chaining Route Handlers
+const uno = (req, res, next) => {
+    console.log('uno');
+    next();
 }
 
+const dos = (req, res, next) => {
+    console.log('dos');
+    next();
+}
 
-const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
-    myEmitter.emit('log', `${req.url}\t${req.method}`, 'reqLog.txt');
+const tres = (req, res, next) => {
+    console.log('tres');
+    res.send("Done!");
+}
 
-    //--Get extension name from url
-    const extension = path.extname(req.url);
+app.get('/chain(.html)?', [uno, dos, tres]);
 
-    //--Then findout what contentType it is and assign to it
-    let contentType;
-    switch (extension) {
-        case '.css':
-            contentType = 'text/css';
-            break;
-        case '.js':
-            contentType = 'text/javascript';
-            break;
-        case '.json':
-            contentType = 'application/json';
-            break;
-        case '.jpg':
-            contentType = 'image/jpeg';
-            break;
-        case '.png':
-            contentType = 'image/png';
-            break;
-        case '.txt':
-            contentType = 'text/plain';
-            break;
-        default:
-            contentType = 'text/html';
-    }
+app.get('/*', (req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html')); //If without specifying status code, it will be 200 since the 404.html file exists
+})
 
-    //--Set filepath
-    let filePath =
-        contentType === 'text/html' && req.url === '/'
-            ? path.join(__dirname, 'views', 'index.html')
-            : contentType === 'text/html' && req.url.slice(-1) === '/'
-                ? path.join(__dirname, 'views', req.url, 'index.html')
-                : contentType === 'text/html'
-                    ? path.join(__dirname, 'views', req.url)
-                    : path.join(__dirname, req.url);
-
-    //--Make .html extension not required in the browser
-    //--Meaning= if extension not found and the last character of the url is not '/'(means its finding a missing file), add .html at the end of the url
-    //--Eg: nodejs-webserver/views/fakeFile > nodejs-webserver/views/fakeFile.html 
-    if (!extension && req.url.slice(-1) !== '/') filePath += '.html';
-
-    const fileExists = fs.existsSync(filePath);
-    if (fileExists) {
-        serveFile(filePath, contentType, res)
-    } else {
-        switch (path.parse(filePath).base) {
-            case 'fakeFile.html':
-                res.writeHead(301, { 'Location': '/new-page.html' });
-                res.end();
-                break;
-            case 'www-page.html':
-                res.writeHead(301, { 'Location': '/' });
-                res.end();
-                break;
-            default:
-                serveFile(path.join(__dirname, 'views', '404.html'), 'text/html', res)
-            //serve 404
-        }
-        //404
-        //301 redirect
-    }
-
-    //-- EXAMPLE OUTPUT for path.parse(filePath)
-    // {
-    //     root: '/',
-    //     dir: '/home/colin/nodejs-test/nodejs-webserver/views',
-    //     base: 'fakeFile.html',
-    //     ext: '.html',
-    //     name: 'fakeFile'
-    // }
-
-});
-
-//--MUST Add a http request listener at the end of .js file
-server.listen(PORT, () => console.log(`Server is listening, running on port ${PORT}`));
-
-//--For practicing emit message for listener to detect 
-// //--Add listener for the log event
-// myEmitter.on('log', (msg) => logEvents(msg));
-//     myEmitter.emit('log', 'Log event emitted');
+app.listen(PORT, () => console.log(`Server is listening, running on port ${PORT}`));
