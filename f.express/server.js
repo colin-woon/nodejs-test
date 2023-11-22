9e4d68d8e083c1b2600198a9a981d9c924c3176e
colin-woon <colinwyj@gmail.com>
2023-11-22 10:25:53 +0800

    Tested middlewares
    - Created custom middleware logger
    - Used built-in middleware for json, form data, images
    - Used third-party middleware CORS

--- f.express/server.js
+++ f.express/server.js
@@ -1,7 +1,40 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
+const { logger } = require('./middleware/logEvents');
 const PORT = process.env.PORT || 3500;
+
+//--Custom middleware logger (must have next param in function, next passes the control to the next handler)
+app.use(logger);
+
+//--Cross Origin Resource Sharing (makes your backend accessible)
+// just add the site name into whitelist for access to backend, typically will delete the two sites before deployment which are local host and custom port as theyre for development purposes only 
+const whitelist = ['https://www.<yoursite>.com', 'http://127.0.0.1:5500', 'http://localhost:3500'];
+// origin in the parameter means the source of request, where it came from
+const corsOption = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1) { //checks if request source is in whitelist
+            callback(null, true); //first param is usually error handler, since its true, it is set to null, the origin source is sent back
+        }
+        else {
+            callback(new Error("Callback not allowed by CORS"))
+        }
+    },
+    optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOption));
+
+//--Built in middleware for url-encoded data/form data
+//--contentType: application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }));
+
+//--Built in middleware for json
+app.use(express.json());
+
+//--Serve static files (eg: images)
+app.use(express.static(path.join(__dirname, '/public')));
 
 //--^ = Begin with
 //--$ = End with
